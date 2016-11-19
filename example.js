b2a7a0e1c8614749afd3384fe11ab251db7030af
Sanemat <o.gata.ken@gmail.com>
2016-11-19 15:08:15 +0900

    :shirt: Fix eslint

--- example.js
+++ example.js
@@ -1,61 +1,61 @@
-'use strict';
+
 
 const express = require('express');
 const simpleOauthModule = require('simple-oauth2');
 
 const app = express();
 const oauth2 = simpleOauthModule.create({
-    client: {
-        id: process.env.CLIENT_ID,
-        secret: process.env.CLIENT_SECRET,
-    },
-    auth: {
-        tokenHost: process.env.TOKEN_HOST,
-    },
+  client: {
+    id: process.env.CLIENT_ID,
+    secret: process.env.CLIENT_SECRET,
+  },
+  auth: {
+    tokenHost: process.env.TOKEN_HOST,
+  },
 });
 
 // Authorization uri definition
 const authorizationUri = oauth2.authorizationCode.authorizeURL({
-    redirect_uri: process.env.REDIRECT_URI,
-    scope: 'read',
-    code: 'code',
+  redirect_uri: process.env.REDIRECT_URI,
+  scope: 'read',
+  code: 'code',
 });
 
 // Initial page redirecting to Github
 app.get('/auth', (req, res) => {
-    console.log(authorizationUri);
-res.redirect(authorizationUri);
+  console.log(authorizationUri);
+  res.redirect(authorizationUri);
 });
 
 // Callback service parsing the authorization token and asking for the access token
 app.get('/callback', (req, res) => {
-    const code = req.query.code;
-const options = {
-    code: code,
+  const code = req.query.code;
+  const options = {
+    code,
     redirect_uri: process.env.REDIRECT_URI,
-};
+  };
 
-oauth2.authorizationCode.getToken(options, (error, result) => {
+  oauth2.authorizationCode.getToken(options, (error, result) => {
     if (error) {
-        console.error('Access Token Error', error.message);
-        return res.json('Authentication failed');
+      console.error('Access Token Error', error.message);
+      return res.json('Authentication failed');
     }
 
     console.log('The resulting token: ', result);
-const token = oauth2.accessToken.create(result);
+    const token = oauth2.accessToken.create(result);
 
-return res
+    return res
     .status(200)
     .json(token);
-});
+  });
 });
 
 app.get('/success', (req, res) => {
-    res.send('');
+  res.send('');
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello<br><a href="/auth">Log in with MF</a>');
+  res.send('Hello<br><a href="/auth">Log in with MF</a>');
 });
 
 app.listen(3000, () => {
