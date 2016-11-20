9563c955873006e96a432193b185976f1e4cbd26
Sanemat <o.gata.ken@gmail.com>
2016-11-20 16:01:41 +0900

    Add phaser example

--- example.js
+++ example.js
@@ -5,6 +5,7 @@ const express = require('express');
 const simpleOauthModule = require('simple-oauth2');
 const template = require('./template.marko');
 
+const path = require('path');
 const app = express();
 const oauth2 = simpleOauthModule.create({
   client: {
@@ -62,6 +63,8 @@ app.get('/', (req, res) => {
     loggedIn: false,
   });
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log('Express server started on port 3000'); // eslint-disable-line
