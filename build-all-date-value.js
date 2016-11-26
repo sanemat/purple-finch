ebddb8d90754212b0bf05e23f0f36dcbbc00b398
Sanemat <o.gata.ken@gmail.com>
2016-11-27 03:37:31 +0900

    :bug: Use moment-timezone

--- build-all-date-value.js
+++ build-all-date-value.js
@@ -1,8 +1,8 @@
-const moment = require('moment');
+const moment = require('moment-timezone');
 
 module.exports = (oldestDate, latestDate) => {
-  const oldestMoment = moment(oldestDate, moment.ISO_8601);
-  const latestMoment = moment(latestDate, moment.ISO_8601);
+  const oldestMoment = moment.tz(oldestDate, moment.ISO_8601, "Asia/Tokyo");
+  const latestMoment = moment.tz(latestDate, moment.ISO_8601, "Asia/Tokyo");
   const result = [];
   for (let targetMoment = oldestMoment; targetMoment.isSameOrBefore(latestMoment, 'day'); targetMoment.add(1, 'days')) {
     result.push({ date: targetMoment.format('YYYY-MM-DD'), value: 0 });
