1e46969cfe3ca7d4e8a00451d4eed7d399f8213e
Sanemat <o.gata.ken@gmail.com>
2016-11-27 03:39:23 +0900

    :shirt: Use single quota

--- build-all-date-value.js
+++ build-all-date-value.js
@@ -1,8 +1,8 @@
 const moment = require('moment-timezone');
 
 module.exports = (oldestDate, latestDate) => {
-  const oldestMoment = moment.tz(oldestDate, moment.ISO_8601, "Asia/Tokyo");
-  const latestMoment = moment.tz(latestDate, moment.ISO_8601, "Asia/Tokyo");
+  const oldestMoment = moment.tz(oldestDate, moment.ISO_8601, 'Asia/Tokyo');
+  const latestMoment = moment.tz(latestDate, moment.ISO_8601, 'Asia/Tokyo');
   const result = [];
   for (let targetMoment = oldestMoment; targetMoment.isSameOrBefore(latestMoment, 'day'); targetMoment.add(1, 'days')) {
     result.push({ date: targetMoment.format('YYYY-MM-DD'), value: 0 });
