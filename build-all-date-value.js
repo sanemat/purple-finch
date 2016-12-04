225651642de56da899a8338b643a61ca0a50adb3
Sanemat <o.gata.ken@gmail.com>
2016-12-04 20:14:50 +0900

    :recycle: Refactor to map

--- build-all-date-value.js
+++ build-all-date-value.js
@@ -3,9 +3,9 @@ const moment = require('moment-timezone');
 module.exports = (oldestDate, latestDate) => {
   const oldestMoment = moment.tz(oldestDate, moment.ISO_8601, 'Asia/Tokyo');
   const latestMoment = moment.tz(latestDate, moment.ISO_8601, 'Asia/Tokyo');
-  const result = [];
+  const result = new Map();
   for (let targetMoment = oldestMoment; targetMoment.isSameOrBefore(latestMoment, 'day'); targetMoment.add(1, 'days')) {
-    result.push({ date: targetMoment.format('YYYY-MM-DD'), value: 0 });
+    result.set(targetMoment.format('YYYY-MM-DD'), { value: 0 });
   }
   return result;
 };
